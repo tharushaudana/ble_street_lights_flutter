c5d4814e40ecfcda3160ed79b8e2543191426fa8
Tharusha Udana <tharushaudana519@gmail.com>
2023-09-09 17:32:21 +0530

    update

--- lib/screens/device/screens/firmwareupdater/firmwareupdate.dart
+++ lib/screens/device/screens/firmwareupdater/firmwareupdate.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
@@ -5,6 +6,7 @@ import 'package:ble_street_lights/bledevice/data.dart';
 import 'package:ble_street_lights/safestate/safestate.dart';
 import 'package:ble_street_lights/screens/device/devicesyncer.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:provider/provider.dart';
 import 'package:ble_street_lights/bledevice/connectionprovider.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +18,10 @@ class DeviceFirmwareUpdaterScreen extends StatefulWidget {
 
 class _DeviceFirmwareUpdaterScreenState
     extends SafeState<DeviceFirmwareUpdaterScreen> {
+  bool infoLoaded = false;
+  bool errorLoadInfo = false;
+  Map? info;
+
   bool firmwareFileDownloaded = false;
   bool isSending = false;
   double sentPercent = 0;
@@ -24,11 +30,68 @@ class _DeviceFirmwareUpdaterScreenState
 
   String? previousVersion;
 
+  _getInfo() async {
+    setState(() {
+      errorLoadInfo = false;
+      infoLoaded = false;
+    });
+
+    Map formData = {
+      "api_key": "tPmAT5Ab3j7F9",
+      "id": "test",
+    };
+
+    final String formBody = formData.entries
+        .map(
+          (e) => e.key + "=" + e.value,
+        )
+        .join("&");
+
+    List<int> bodyBytes = utf8.encode(formBody); // utf8 encode
+
+    try {
+      final request = await HttpClient().postUrl(
+        Uri.parse("https://appstreetlight.000webhostapp.com/info.php"),
+      );
+
+      request.headers.set('Content-Type', "application/x-www-form-urlencoded");
+      request.headers.set('Content-Length', bodyBytes.length.toString());
+      request.add(bodyBytes);
+
+      HttpClientResponse res = await request.close();
+
+      if (res.statusCode != 200) {
+        setState(() {
+          errorLoadInfo = true;
+        });
+
+        return;
+      }
+
+      final bytes = await consolidateHttpClientResponseBytes(res);
+      String body = utf8.decode(bytes);
+
+      info = json.decode(body);
+
+      setState(() {
+        infoLoaded = true;
+      });
+    } catch (e) {
+      log("Info Load Error: $e");
+
+      setState(() {
+        errorLoadInfo = true;
+      });
+    }
+  }
+
   Future<Uint8List> _downloadFirmwareFile() async {
     //final req = await HttpClient().getUrl(Uri.parse("https://tmpfiles.org/dl/2274759/esp32_f1.0v.ino.esp32.bin"));
-    final req = await HttpClient().getUrl(Uri.parse("https://appstreetlight.000webhostapp.com/firmware/F1.3V.bin"));
+    final req = await HttpClient().getUrl(Uri.parse(
+        "https://appstreetlight.000webhostapp.com/firmware/F1.3V.bin"));
     final res = await req.close();
     final bytes = await consolidateHttpClientResponseBytes(res);
+
     return bytes;
   }
 
@@ -84,6 +147,7 @@ class _DeviceFirmwareUpdaterScreenState
   @override
   void initState() {
     super.initState();
+    _getInfo();
   }
 
   @override
@@ -100,7 +164,45 @@ class _DeviceFirmwareUpdaterScreenState
 
       late Widget view;
 
-      if (version != null &&
+      //##################### DOWNLOAD THE INFORMATIONS #####################
+
+      if (!infoLoaded && !errorLoadInfo) {
+        view = const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(height: 10),
+            Text("Checking..."),
+          ],
+        );
+      } else if (errorLoadInfo) {
+        view = Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.warning_amber_rounded,
+              size: 90,
+              color: Colors.red,
+            ),
+            const SizedBox(height: 10),
+            const Text("Unable to retrive firmware update informations.", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold,),),
+            const SizedBox(height: 5),
+            const Text("Make sure you have turn on mobile data or wifi.", style: TextStyle(fontSize: 12),),
+            TextButton(
+              onPressed: () {
+                _getInfo();
+              },
+              child: const Text("TRY AGAIN"),
+            ),
+          ],
+        );
+      }
+
+      //##################### OTA UPDATED DONE #####################
+
+      else if (version != null &&
           previousVersion != null &&
           version != previousVersion) {
         view = Column(
@@ -123,131 +225,159 @@ class _DeviceFirmwareUpdaterScreenState
             Text("updated ${previousVersion!} to $version"),
           ],
         );
-      } else {
-        if (state == null) {
-          view = const Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("[Details Not Received]"),
-            ],
-          );
-        }
+      }
 
-        if (state == BLEDeviceData.OTA_STA_READY) {
-          view = Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              !firmwareFileDownloaded
-                  ? FilledButton(
-                      onPressed: () async {
-                        firmwareBytes = await _downloadFirmwareFile();
+      //##################### OTA STATES (BEFORE DONE) #####################
 
-                        setState(() {
-                          firmwareFileDownloaded = true;
-                        });
-                      },
-                      child: Text("Download Firmware File"),
-                    )
-                  : FilledButton(
-                      onPressed: () {
-                        _startUpdate(provider);
-                      },
-                      child: Text("Update Now"),
-                    ),
-              firmwareBytes != null
-                  ? Text(
-                      "Download successfully. ${firmwareBytes!.length} bytes")
-                  : Container(),
-            ],
-          );
-        }
-
-        if (state == BLEDeviceData.OTA_STA_RECEIVING) {
-          view = Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Sending the file..."),
-              const SizedBox(height: 10),
-              LinearProgressIndicator(
-                value: sentPercent,
+      else if (state == null) {
+        view = const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.warning_amber_rounded,
+              size: 90,
+              color: Colors.red,
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Current firmware details not received for the device. It seems like there has some connection issues.",
+              style: TextStyle(
+                color: Colors.red,
               ),
-            ],
-          );
-        }
-
-        if (state == BLEDeviceData.OTA_STA_RECEIVED) {
-          view = const Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "File Successfully Received",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
-                ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        );
+      } 
+      else if (double.parse(version!) == double.parse(info!["available_version"])) {
+        view = Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              version,
+              style: const TextStyle(
+                color: Colors.blue,
+                fontSize: 70,
+                fontFamily: "monospace",
+                fontWeight: FontWeight.bold,
               ),
-              Text("starting OTA update..."),
-            ],
-          );
-        }
-
-        if (state == BLEDeviceData.OTA_STA_UPDATING) {
-          view = const Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "OTA Updating...",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            const Text(
+              "Already Latest Version",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 25,
               ),
-            ],
-          );
-        }
+            ),
+            const SizedBox(height: 6),
+            const Text("Update not available yet.", style: TextStyle(fontSize: 12),),
+          ],
+        ).animate().fade(duration: 500.ms);
+      }
+      else if (state == BLEDeviceData.OTA_STA_READY) {
+        view = Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            !firmwareFileDownloaded
+                ? FilledButton(
+                    onPressed: () async {
+                      firmwareBytes = await _downloadFirmwareFile();
 
-        if (state == BLEDeviceData.OTA_STA_REBOOTING) {
-          view = const Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Rebooting The Device...",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
-                ),
+                      setState(() {
+                        firmwareFileDownloaded = true;
+                      });
+                    },
+                    child: Text("Download Firmware File"),
+                  )
+                : FilledButton(
+                    onPressed: () {
+                      _startUpdate(provider);
+                    },
+                    child: Text("Update Now"),
+                  ),
+            firmwareBytes != null
+                ? Text("Download successfully. ${firmwareBytes!.length} bytes")
+                : Container(),
+          ],
+        );
+      } else if (state == BLEDeviceData.OTA_STA_RECEIVING) {
+        view = Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Sending the file..."),
+            const SizedBox(height: 10),
+            LinearProgressIndicator(
+              value: sentPercent,
+            ),
+          ],
+        );
+      } else if (state == BLEDeviceData.OTA_STA_RECEIVED) {
+        view = const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "File Successfully Received",
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
               ),
-              Text("looking for re-connecting"),
-            ],
-          );
-        }
-
-        if (state == BLEDeviceData.OTA_STA_ERROR) {
-          view = Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Error",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            Text("starting OTA update..."),
+          ],
+        );
+      } else if (state == BLEDeviceData.OTA_STA_UPDATING) {
+        view = const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "OTA Updating...",
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
               ),
-              Text(provider.deviceData.otaValue("e", "[no description]")),
-            ],
-          );
-        }
+            ),
+          ],
+        );
+      } else if (state == BLEDeviceData.OTA_STA_REBOOTING) {
+        view = const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Rebooting The Device...",
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text("looking for re-connecting"),
+          ],
+        );
+      } else if (state == BLEDeviceData.OTA_STA_ERROR) {
+        view = Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Error",
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(provider.deviceData.otaValue("e", "[no description]")),
+          ],
+        );
       }
 
       return Scaffold(
@@ -260,7 +390,7 @@ class _DeviceFirmwareUpdaterScreenState
           padding: EdgeInsets.all(20),
           child: Column(
             children: [
-              Row(
+              /*Row(
                 children: [
                   const Text(
                     "Current Version:",
@@ -277,7 +407,7 @@ class _DeviceFirmwareUpdaterScreenState
                     ),
                   ),
                 ],
-              ),
+              ),*/
               Expanded(
                 child: view,
               ),
